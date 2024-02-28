ab838ab657c03ba10ff669777385f9b2db22618e
Sajif <sajifdevapalan@outlook.com>
2024-02-28 21:57:30 +0000

    src/App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 
+import './App.css';
+import { Footer } from './components/footer/footer.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-   
-      </header>
+      <Footer/>
     </div>
   );
 }
--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,10 +1,10 @@
-import "./Footer.css";
+import "./footer.css";
 
 
 export const Footer = () => {
     return (
     <div className="Footer">
-        <p className="foot" >@Danny</p>
+        <p className="foot" >@Daniel</p>
     </div> 
     )
 }
